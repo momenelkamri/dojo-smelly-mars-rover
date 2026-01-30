d71f11d97999ee984fd0a0c68587c0bb0ed6345c
NSWPOL\Momen.elKamri_nsp <momenelkamri@hotmail.com>
2026-01-30 09:53:10 +0100

    refactor: simplify coordinate management in RoverState by consolidating x and y properties

--- src/RoverState.ts
+++ src/RoverState.ts
@@ -19,24 +19,23 @@ type Coordinate = {
 };
 
 export class RoverState {
-    // coordinate: { x: number; y: number } = { x: 0, y: 0 };
-
-    private xCoordinate: number;
-    private yCoordinate: number;
+    private coordinate: Coordinate;
     currentHeading: Heading;
 
     constructor(xCoordinate: number, yCoordinate: number, currentHeading: Heading) {
-        this.xCoordinate = xCoordinate;
-        this.yCoordinate = yCoordinate;
+        this.coordinate = { 
+            x: xCoordinate, 
+            y: yCoordinate,
+        };
+
         this.currentHeading = currentHeading;
     }
 
     public setCoordinate(coordinate: Coordinate): void {
-        this.xCoordinate = coordinate.x;
-        this.yCoordinate = coordinate.y;
+        this.coordinate = coordinate;
     }
 
     public getCoordinate(): Coordinate {
-        return { x: this.xCoordinate, y: this.yCoordinate };
+        return this.coordinate;
     }
 } 
